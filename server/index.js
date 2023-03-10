8482a112e8bd8756a3ccfcc8e8bacacafc02b31e
Uma-Shankar0927 <67121111+Uma-Shankar0927@users.noreply.github.com>
2023-03-11 01:18:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use('/api/v1/post',postRoutes);
 app.use('/api/v1/dalle',dalleRoutes);
 
 app.get('/', async(req,res) => {
-    res.send('Hello from DALL-E');
+    res.sendStatus(200);
 })
 
 const startServer = async () => {
